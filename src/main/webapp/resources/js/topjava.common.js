67029ba54b1b44286a87c3166a7a717b0ef50bbc
Oleg Ivanov <io2357911@yandex.ru>
2020-11-28 17:22:56 +0300

    HW08: Fix id duplicates

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -4,7 +4,7 @@ function makeEditable() {
     form = $('#detailsForm');
     $(".delete").click(function () {
         if (confirm('Are you sure?')) {
-            deleteRow($(this).attr("id"));
+            deleteRow($(this).parents("tr").attr("id"));
         }
     });
 
