e164b05dfb33570a1e1e480489eb04f005e3e712
Oleg Ivanov <io2357911@yandex.ru>
2020-12-12 22:44:55 +0300

    HW10: Implement locale selection

--- src/main/webapp/resources/js/topjava.meals.js
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -65,6 +65,8 @@ $(function () {
         },
     });
 
+    $.datetimepicker.setLocale(localeCode);
+
 //  http://xdsoft.net/jqplugins/datetimepicker/
     var startDate = $('#startDate');
     var endDate = $('#endDate');
